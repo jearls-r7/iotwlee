a217966610acf5c776a7e385ec65c03b519355bc
James Earls <69523003+jearls-r7@users.noreply.github.com>
2024-02-07 11:37:12 +0000

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
+  basePath: "/<your_repo_name>",
+  images: {
+    unoptimized: true,
+  },
 };
-
-export default nextConfig;
